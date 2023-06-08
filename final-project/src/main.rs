b4f4a6dac2fd13ae56b026db2f4ef75d340bb5eb
IVIURARY <haydn.murray1@yahoo.co.uk>
2023-06-08 08:11:20 +0100

    graceful shutdown

--- final-project/src/main.rs
+++ final-project/src/main.rs
@@ -9,14 +9,14 @@ fn main() {
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        let steam = stream.unwrap();
+        let stream = stream.unwrap();
 
         pool.execute(|| {
-            handle_connection(steam);
+            handle_connection(stream);
         });
     }
 
-    println!("Done!");
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
